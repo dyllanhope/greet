46b683cae81ba9900881518984e4e976fada1fb6
Dyllan Hope <dyllanjhope@gmail.com>
2019-05-30 10:16:47 +0200

    initial commit

---
+++ greet_dyllan.js
@@ -0,0 +1,19 @@
+// const chalk = require('chalk');
+
+const greet = require('./greet');
+
+var figlet = require('figlet');
+
+figlet.text(greet('Dyllan'), {
+	font: 'ANSI Shadow',
+    horizontalLayout: 'default',
+    verticalLayout: 'default'
+},function(err, data){
+	if(err){
+		console.log('Something went wrong...');
+		console.log(err);
+		return;
+	}	
+	console.log(data)
+})
+
