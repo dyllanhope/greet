5fe0d29807f9705c8e49aa58a0d88012ccabe903
Dyllan Hope <dyllanjhope@gmail.com>
2019-05-30 11:59:27 +0200

    minor formatting changes

--- greet_dyllan.js
+++ greet_dyllan.js
@@ -1,20 +1,18 @@
 "use strict";
 const chalk = require('chalk');
-
 const greet = require('./greet');
-
-var figlet = require('figlet');
+const figlet = require('figlet');
 
 figlet.text(greet('Dyllan'), {
-	font: 'ANSI Shadow',
-    horizontalLayout: 'default',
-    verticalLayout: 'default'
-},function(err, data){
-	if(err){
-		console.log('Something went wrong...');
-		console.log(err);
-		return;
-	}	
-	console.log(chalk.bgBlack.green(data));
-})
+		font: 'ANSI Shadow',
+		horizontalLayout: 'default',
+		verticalLayout: 'default'
+	}, function (err, data) {
+		if (err) {
+			console.log('Something went wrong...');
+			console.log(err);
+			return;
+		}
+		console.log(chalk.bgBlack.green(data));
+	});
 
